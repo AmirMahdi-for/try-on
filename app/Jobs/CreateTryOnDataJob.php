a8521e3d356e857cad461de5ba52bdd817c767ec
AmirMahdi-for <amirmahdifor@gmail.com>
2025-02-23 23:18:52 -0800

    feat: add create and update job of try on

---
+++ app/Jobs/CreateTryOnDataJob.php
@@ -0,0 +1,44 @@
+<?php 
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use TryOn\Models\TryOn;
+
+class CreateTryOnDataJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $data;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle()
+    {
+        TryOn::create([
+            'uuid' => $this->data['uuid'],
+            'user_id' => $this->data['user_id'],
+            'model_file_id' => $this->data['model_file_id'],
+            'garment_file_id' => $this->data['garment_file_id'],
+            'category' => $this->data['category'],
+            'send_images_result' => $this->data['send_images_result'],
+            'send_images_status_code' => $this->data['send_images_status_code'],
+        ]);
+    }
+
+}
---
+++ app/Jobs/UpdateTryOnDataJob.php
@@ -0,0 +1,38 @@
+<?php 
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use TryOn\Models\TryOn;
+
+class UpdateTryOnDataJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $data;
+    protected $uuid;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(string $uuid, array $data)
+    {
+        $this->data = $data;
+        $this->uuid = $uuid;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle()
+    {
+        TryOn::where('uuid', $this->uuid)->update($this->data);
+    }
+
+}

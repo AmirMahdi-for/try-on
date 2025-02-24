2d78ba569529c12d3d9cb694c00e5ebdf3c5d5d6
AmirMahdi-for <amirmahdifor@gmail.com>
2025-02-23 22:58:55 -0800

    feat: add repositories

---
+++ app/Repositories/TryOnRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace TryOn\Repositories;
+
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Storage;
+use TryOn\Http\Requests\VoiceFileRequest;
+use TryOn\Repositories\Interfaces\TryOnRepositoryInterface;
+
+class TryOnRepository implements TryOnRepositoryInterface
+{
+    
+    
+}
---
+++ app/TryOnServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace TryOn\Providers;
+
+use TryOn\Repositories\Interfaces\TryOnRepositoryInterface;
+use TryOn\Repositories\TryOnRepository;
+use Illuminate\Support\ServiceProvider;
+
+class TryOnServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../../config/try-on.php', 'try-on');
+        
+        $this->app->bind(TryOnRepositoryInterface::class, TryOnRepository::class);
+    }
+
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__ . '/../../config/try-on.php' => config_path('try-on.php'),
+        ], 'config');
+        
+        $this->loadRoutesFrom(__DIR__ . '/../Routes/api.php');
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/migrations');
+    }
+}

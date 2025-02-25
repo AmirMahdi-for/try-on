f12f68d7cc78ed78b0019fda6af2c5f9602ffb22
AmirMahdi-for <amirmahdifor@gmail.com>
2025-02-24 23:37:28 -0800

    fix: key of services in config

--- config/try-on.php
+++ config/try-on.php
@@ -2,9 +2,8 @@
 
 
 return [
-    'api_key' => env('SPEEECH_TEXTER_X_API_KEY'),
-    'voice_api' => env('SPEEECH_TEXTER_VOICE_API'),
-    'user_model' => env('SPEEECH_TEXTER_USER_MODEL', 'App\Models\User'),
-    'file_model' => env('SPEEECH_TEXTER_FILE_MODEL', 'App\Models\File'),
-    'prefix' => 'speeech-texter',
+    'cateogry-indetifier-api' => env('CATEGORY_IDENTIFIER_API'),
+    'cateogry-indetifier-token' => env('CATEGORY_IDENTIFIER_TOKEN'),
+    'try-on-token' => env('TRY_ON_SERVICE_TOKEN'),
+    'try-on-api' => env('TRY_ON_SERVICE_API'),
 ];
